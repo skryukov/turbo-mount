49a2153255d874cf0875643ab0c494a04e6036ee
Svyatoslav Kryukov <me@skryukov.dev>
2024-11-26 12:15:53 +0300

    Support shakapacker

--- lib/generators/turbo_mount/install_generator.rb
+++ lib/generators/turbo_mount/install_generator.rb
@@ -25,6 +25,9 @@ module TurboMount
         enum: JSPackageManager.package_managers,
         desc: "The package manager you want to use to install Turbo Mount"
 
+      class_option :interactive, type: :boolean, default: true,
+        desc: "Whether to prompt for optional installations"
+
       class_option :verbose, type: :boolean, default: false,
         desc: "Run the generator in verbose mode"
 
@@ -33,6 +36,7 @@ module TurboMount
 
         package_manager.validate!
 
+        create_initializer
         if package_manager.importmap?
           install_importmap
         else
@@ -44,35 +48,39 @@ module TurboMount
 
       private
 
-      def install_nodejs
-        package_manager.add_dependencies("turbo-mount", FRAMEWORKS[framework][:npm_packages])
-
+      def create_initializer
         say "Creating Turbo Mount initializer"
-        template "turbo-mount.js", js_file_path("turbo-mount.js")
+
+        needs_alias = shakapacker? || package_manager.importmap?
+        initializer_path = needs_alias ? "turbo-mount-initializer.js" : "turbo-mount.js"
+        initializer_import = needs_alias ? %(import "turbo-mount-initializer"\n) : %(import "./turbo-mount"\n)
+
+        template "turbo-mount.js", js_file_path(initializer_path)
         begin
-          append_to_file js_entrypoint, %(import "./turbo-mount"\n)
+          append_to_file js_entrypoint, initializer_import
         rescue
-          say 'Could not find the application entrypoint, please add `import "./turbo-mount"` manually.', :yellow
+          say "Could not find the application entrypoint, please add `#{initializer_import.strip}` manually.", :yellow
         end
+      end
+
+      def install_nodejs
+        package_manager.add_dependencies("turbo-mount", FRAMEWORKS[framework]["npm_packages"])
+
         warn_about_vite_plugin if vite?
       end
 
       def install_importmap
-        say "Creating Turbo Mount initializer"
-        template "turbo-mount.js", js_file_path("turbo-mount-initializer.js")
-        append_to_file "app/javascript/application.js", %(import "turbo-mount-initializer"\n)
-
         say "Pinning Turbo Mount to the importmap"
         append_to_file "config/importmap.rb", %(pin "turbo-mount", to: "turbo-mount.min.js"\n)
         append_to_file "config/importmap.rb", %(pin "turbo-mount/#{framework}", to: "turbo-mount/#{framework}.min.js"\n)
         append_to_file "config/importmap.rb", %(pin "turbo-mount-initializer"\n)
 
         say "Pinning framework dependencies to the importmap"
-        package_manager.add_dependencies(FRAMEWORKS[framework][:pins])
+        package_manager.add_dependencies(FRAMEWORKS[framework]["pins"])
       end
 
       def warn_about_vite_plugin
-        say "Make sure to install and add #{FRAMEWORKS[framework][:vite_plugin]} to your Vite config", :yellow
+        say "Make sure to install and add #{FRAMEWORKS[framework]["vite_plugin"]} to your Vite config", :yellow
       end
 
       def package_manager
@@ -80,7 +88,7 @@ module TurboMount
       end
 
       def extension
-        FRAMEWORKS[framework][:extension]
+        FRAMEWORKS[framework]["extension"]
       end
 
       def framework
